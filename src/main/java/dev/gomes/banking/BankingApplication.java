e5b7f78bc5dc03e2ba08ec648f293b0501932137
Cleber Gomes <fclebinho@gmail.com>
2024-06-06 14:08:28 -0300

    fix: CORS

--- src/main/java/dev/gomes/banking/BankingApplication.java
+++ src/main/java/dev/gomes/banking/BankingApplication.java
@@ -2,24 +2,11 @@ package dev.gomes.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("*");
-			}
-		};
 	}
 }
---
+++ src/main/java/dev/gomes/banking/config/WebConfig.java
@@ -0,0 +1,17 @@
+package dev.gomes.banking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+    // .allowedOrigins("*")
+    // .allowedMethods("GET", "POST", "PUT", "DELETE");
+  }
+
+}
