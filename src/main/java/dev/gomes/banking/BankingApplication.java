d78abe6ed5db8397a5bcb7d5ad276631c276da9d
Cleber Gomes <fclebinho@gmail.com>
2024-06-06 13:56:55 -0300

    fix: Cors

--- src/main/java/dev/gomes/banking/BankingApplication.java
+++ src/main/java/dev/gomes/banking/BankingApplication.java
@@ -2,6 +2,9 @@ package dev.gomes.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BankingApplication {
@@ -10,4 +13,13 @@ public class BankingApplication {
 		SpringApplication.run(BankingApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("*");
+			}
+		};
+	}
 }
